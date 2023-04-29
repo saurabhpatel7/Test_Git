6f7f88ac0f7b6b2ea40426b23358cd81dcce689c
saurabhpatel7 <saurabh.patel2021@gmail.com>
2023-04-30 02:19:31 +0530

    Added test.js file

---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Hello I'm Devil")
+console.log("Hello I'm Devil")
+console.log("Hello I'm Devil")
+console.log("Hello I'm Devil")
+console.log("Hello I'm Devil")
+console.log("Hello I'm Devil")
